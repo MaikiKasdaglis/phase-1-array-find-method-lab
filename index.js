cbed3a39f2c913a13133a3ebefb31fe971d04354
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-06-16 17:08:00 -0600

    finishes lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+    //...
+  ]
+  
+  
+ // THIS CODE KINDA WORKS  
+//   function superbowlWin(array) {
+//     let result = array.find(game => game.result == "W")
+//     return result['year'] 
+// }
+
+
+function superbowlWin(array) {
+    let result = array.find(game => game.result == "W")
+    console.log(result)
+    return result ? result.year : undefined
+}
+superbowlWin(record)
